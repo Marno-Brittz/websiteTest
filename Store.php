2d767ec86b4b55bfd87681eb7e9aa2a5a1020e1c
Marno-Brittz <marnobrittz@gmail.com>
2024-06-22 15:24:51 +0200

    Add files via upload

---
+++ Store.php
@@ -0,0 +1,30 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+    
+<head>
+    <link rel = "stylesheet" href = "StyleSheet1.css"/>
+    <script src = "Jscript1.js"></script>
+    <meta charset="utf-8" />
+    <meta name = "viewport" content = "width=device-width, initail-scale=1.0"/>
+    <meta name = "description" content = "We offer various coding service from HTML to C#"/>
+    <meta name = "keywords" content = "Coding, C#, HTML, Javascript, C++, Java"/>
+</head>
+
+    
+<body id = "Body">
+
+<?php include "incudes/nav.php"?>
+<?php include "incudes/Header.php"?>
+
+
+
+
+<main></main>
+
+<?php include "incudes/Footer.php"?>
+
+
+    
+</body>
+</html>
---
+++ Table Test.sql
@@ -0,0 +1,1 @@
+CREATE TABLE TEST( Row1 VarChar(255),Row2 VarChar(255),Row3 VarChar(255),Row4 VarChar(255));
